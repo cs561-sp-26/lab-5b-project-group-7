65aef5bdad77937cdf3e43cc152ddcfb11d84510
Daniel Kish <kishda@oregonstate.edu>
2026-05-06 00:11:16 -0500

    confirm delete branch

--- scripts/roundsMode.js
+++ scripts/roundsMode.js
@@ -216,18 +216,7 @@ function updateRoundInTable(rowIndex) {
 const thisRound = document.getElementById("r-" + GlobalUserData.rounds[rowIndex].roundNum);
 writeRoundToTable(thisRound,rowIndex);
 }
-/*************************************************************************
- * @function deleteRound
- * @desc
- * Deletes a round from the "Rounds" table and from local storage
- * @param roundId -- the unique id of the round to be deleted
- * @returns -- true if round could be deleted, false otherwise
- *************************************************************************/
-function deleteRound(roundId) {
-    GlobalUserData.rounds = GlobalUserData.rounds.filter(function (round) {
-        return round.roundNum !== roundId;
-    });
-}
+
 /*************************************************************************
  * @function confirmDelete
  * @desc
